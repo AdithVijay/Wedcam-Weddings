d0afd7704dc3c339b76e20280387672525ed730e
ARJUN-SREEKUMAR <72598741+ARJUN-SREEKUMAR@users.noreply.github.com>
2024-04-18 18:54:57 +0530

    test commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({description,images}) => {
   return (
     <div>
         <NavLink to="/menu">
-            <div className=' flex  flex-col m-6 '>
+            <div className=' flex b flex-col m-6 '>
                 <div >
                     <img className='hover:scale-105 transition   duration-1000   h-96 lg:h-[30rem] object-cover overflow-hidden rounded-xl  min-w-[100%]' src={images} alt="" />
                 </div>
